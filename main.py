70b257f36300422eb4c11f2ce976c39d10ba1642
overstac <105587254+overstac@users.noreply.github.com>
2022-07-06 17:35:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 
-root_dir = Path("files")
-file_paths= root_dir.iterdir()
 
-for path in file_paths:
-  new_filename= "new-" + path.stem + path.suffix
-  new_filepath = path.with_name(new_filename)
-  print(new_filepath)
-  path.rename(new_filepath)
+                            solution 1- Noob
+root_dir= Path("files")
+file_path= root_dir.glob("**/*")
+                              
+for path in file_path:
+  if path.is_file():
+    txt= path.parts[-1]
+    txt_update= txt.replace("txt", "csv")
+    new_filepath= path.with_name(txt_update)
+    path.rename(new_filepath)
+
+                           solution 2- Pro
+root_dir= Path("files")
+for path in root_dir.rglob("**/*"):
+  if path.is_file():
+    new_filepath= path.with_suffix(".csv")
+    path.rename(new_filepath)
